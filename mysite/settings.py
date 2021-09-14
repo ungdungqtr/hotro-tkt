c6f48e77455c2bce5a8767b0d6bfbca94e60ce8d
unknown <Administrator@QTR-NDNTAM.mb.tct.vn>
2021-09-14 11:07:42 +0700

    demo

--- mysite/settings.py
+++ mysite/settings.py
@@ -132,11 +132,11 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
 os.path.join(BASE_DIR, 'static'),
 )
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-X_FRAME_OPTIONS = 'SAMEORIGIN'
+# X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 
 # Default primary key field type
