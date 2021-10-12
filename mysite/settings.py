499c17b4903d2c11c3559967adcf5563f25fa9a6
ungdungqtr <90811901+ungdungqtr@users.noreply.github.com>
2021-10-12 08:11:31 +0700

    create database qtr-tkt-2

--- mysite/settings.py
+++ mysite/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'tkt-qtr',
+        'NAME': 'tkt-qtr-2',
         'USER': 'postgres',
         'PASSWORD': 'cntt-qtr',
         'HOST': 'localhost',
