6cb4000acc96ec561dcd6755abe4bbf06a9a017e
ungdungqtr <90811901+ungdungqtr@users.noreply.github.com>
2024-03-15 10:09:00 +0700

    correct sau_hoan_gtgt

--- tkt_qtr/views.py
+++ tkt_qtr/views.py
@@ -551,7 +551,7 @@ def ktra_sau_hoan(request):
             '<ld_cuc_ten>' : ld_cuc.ld_ten,
             '<hinh_thuc_ky>' : ky_ten[ld_cuc.ld_cv.upper()],
         }       
-        QD = process_data.lap_qd_ktra_hoan_gtgt(tt_qd, doan_ktra)
+        QD = process_data.lap_qd_ktra_sau_hoan_gtgt(tt_qd, doan_ktra)
         QD.empty_media()
         file_path = [QD.to_trinh(), QD.qd_ktra(), QD.qd_gsat(), QD.kh_gsat()]
         zip_path = os.path.join(settings.STATICFILES_DIRS[0], "media_store", mst + "_QD_ktra_sau_hoan.zip")
