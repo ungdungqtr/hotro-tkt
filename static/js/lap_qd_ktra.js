606f8317f6c1bfe38b8c40d19e5adf9b5c4c169b
ungdungqtr <90811901+ungdungqtr@users.noreply.github.com>
2025-08-04 21:07:22 +0700

    test

--- static/js/lap_qd_ktra.js
+++ static/js/lap_qd_ktra.js
@@ -4,7 +4,17 @@ $(document).ready(function () {
         delimiter: '/',
         datePattern: ['d','m','Y']
     });
-    new Cleave('.trinh_ky', '.tu_ngay', '.den_ngay', {
+    new Cleave('.trinh_ky', {
+        date: true,
+        delimiter: '/',
+        datePattern: ['d','m','Y']
+    });
+    new Cleave('.tu_ngay', {
+        date: true,
+        delimiter: '/',
+        datePattern: ['d','m','Y']
+    });
+    new Cleave('.den_ngay', {
         date: true,
         delimiter: '/',
         datePattern: ['d','m','Y']
